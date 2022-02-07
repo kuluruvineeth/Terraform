b083c6c7f2571bd60a397b0c61c1dc39ae049747
kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-02-07 06:51:14 +0530

    slight changes

--- main.tf
+++ main.tf
@@ -27,11 +27,11 @@ provider "aws" {
 #     tags = {
 #       Name = "prod-subnet1"
 #     }
-  
+
 # }
 
 variable "subnet_prefix" {
-    description = "cidr block for the subnet"
+  description = "cidr block for the subnet"
 }
 
 
@@ -40,67 +40,67 @@ variable "subnet_prefix" {
 # 1. Create vpc
 
 resource "aws_vpc" "prod-vpc" {
-    cidr_block = "10.0.0.0/16"
-    tags = {
-        Name = "production"
-    }
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "production"
+  }
 }
 
 # 2. Create Internet Gateway
 
 resource "aws_internet_gateway" "gw" {
-    vpc_id = aws_vpc.prod-vpc.id 
+  vpc_id = aws_vpc.prod-vpc.id
 }
 
 # 3. Create Custom Route Table
 
 resource "aws_route_table" "prod-route-table" {
-    vpc_id = aws_vpc.prod-vpc.id
+  vpc_id = aws_vpc.prod-vpc.id
 
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.gw.id
-    }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
 
-    route {
-        ipv6_cidr_block = "::/0"
-        gateway_id = aws_internet_gateway.gw.id
-    }
+  route {
+    ipv6_cidr_block = "::/0"
+    gateway_id      = aws_internet_gateway.gw.id
+  }
 
-    tags = {
-        Name = "Prod"
-    }
-  
+  tags = {
+    Name = "Prod"
+  }
+
 }
 
 # 4. Create a Subnet
 
 resource "aws_subnet" "subnet-1" {
-    vpc_id = aws_vpc.prod-vpc.id
-    cidr_block = var.subnet_prefix[0].cidr_block
-    availability_zone = "us-east-1a"
+  vpc_id            = aws_vpc.prod-vpc.id
+  cidr_block        = var.subnet_prefix[0].cidr_block
+  availability_zone = "us-east-1a"
 
-    tags = {
-        Name = var.subnet_prefix[0].name
-    }
+  tags = {
+    Name = var.subnet_prefix[0].name
+  }
 }
 
 resource "aws_subnet" "subnet-2" {
-    vpc_id = aws_vpc.prod-vpc.id
-    cidr_block = var.subnet_prefix[1].cidr_block
-    availability_zone = "us-east-1a"
+  vpc_id            = aws_vpc.prod-vpc.id
+  cidr_block        = var.subnet_prefix[1].cidr_block
+  availability_zone = "us-east-1a"
 
-    tags = {
-        Name = var.subnet_prefix[1].name
-    }
+  tags = {
+    Name = var.subnet_prefix[1].name
+  }
 }
 
 
 # 5. Associate subnet with Route Table
 
 resource "aws_route_table_association" "a" {
-    subnet_id = aws_subnet.subnet-1.id
-    route_table_id = aws_route_table.prod-route-table.id
+  subnet_id      = aws_subnet.subnet-1.id
+  route_table_id = aws_route_table.prod-route-table.id
 }
 
 # 6. Create Security Group to allow port 22,80,443
@@ -111,27 +111,27 @@ resource "aws_security_group" "allow_web" {
   vpc_id      = aws_vpc.prod-vpc.id
 
   ingress {
-    description      = "HTTPS"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    description      = "HTTP"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    description      = "SSH"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
@@ -161,38 +161,38 @@ resource "aws_eip" "one" {
   vpc                       = true
   network_interface         = aws_network_interface.web-server-nic.id
   associate_with_private_ip = "10.0.1.50"
-  depends_on = [aws_internet_gateway.gw]
+  depends_on                = [aws_internet_gateway.gw]
 }
 
 # 9. Create Ubuntu server and Install/enable apache2
 
 resource "aws_instance" "ubuntu" {
-    ami = "ami-0e472ba40eb589f49"
-    instance_type = "t2.micro"
-    availability_zone = "us-east-1a"
-    key_name = "main-key"
+  ami               = "ami-0e472ba40eb589f49"
+  instance_type     = "t2.micro"
+  availability_zone = "us-east-1a"
+  key_name          = "main-key"
 
-    network_interface {
-        device_index = 0
-        network_interface_id = aws_network_interface.web-server-nic.id
-    }
+  network_interface {
+    device_index         = 0
+    network_interface_id = aws_network_interface.web-server-nic.id
+  }
 
-    user_data = <<-EOF
+  user_data = <<-EOF
                   #!/bin/bash
                   sudo apt update -y
                   sudo apt install apache2 -y
                   sudo sytemctl start apache2
                   sudo bash -c 'echo your very first web server > /var/www/html/index.html'
                   EOF
-    tags = {
-        Name = "web-server"
-    }
+  tags = {
+    Name = "web-server"
+  }
 }
 
 output "server_public_ip" {
-    value = aws_eip.one.public_ip
+  value = aws_eip.one.public_ip
 }
 
 output "server_id" {
-    value = aws_instance.ubuntu.id
+  value = aws_instance.ubuntu.id
 }
