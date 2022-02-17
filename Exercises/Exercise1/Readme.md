bdde9aaf23ebbab07b1eb0b264eff582d2a4f443
kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-02-17 15:05:47 +0530

    exercise 1

---
+++ Exercises/Exercise1/Readme.md
@@ -0,0 +1,5 @@
+# Exercise1 - Launching EC2 instance
+- Write first_instance.tf file
+- Launch Instance
+- Make some changes to first_instance.tf file
+- Apply changes.
---
+++ Exercises/Exercise1/first_instance.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = "us-east-2"
+  # access_key = ""
+  # secret_key = ""
+}
+
+resource "aws_instance" "intro" {
+  ami                    = "ami-03657b56516ab7912"
+  instance_type          = "t2.micro"
+  availability_zone      = "us-east-2a"
+  key_name               = "intro-key"
+  vpc_security_group_ids = ["sg-0780815f55104be8a"]
+  tags = {
+    Name    = "Inro-Instance"
+    Project = "Intro"
+  }
+
+}
